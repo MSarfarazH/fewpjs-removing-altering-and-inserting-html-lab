63d0320b2c1beee3c6f512087b6ae29beda6bfbb
MSarfarazH <m.s.hussain@outlook.com>
2020-11-22 11:33:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+// document.body.appendChild()
+
+let main = document.querySelector("#main");
+main.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Mohammad is the champion"
+
+
+
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }
+
+// element.appendChild(ul)
+
+// element.innerHTML = 'Hello, DOM!'
+
+// element.style.backgroundColor = '#27647B';
+
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left'
